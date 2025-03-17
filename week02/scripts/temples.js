ecc021514bc6e538652f18e87d1c9fa4130a6fee
jdc1731 <jkeil@byupathway.edu>
2025-03-16 19:27:23 -0600

    mix up

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -4,3 +4,17 @@ document.querySelector('#current-year').textContent = `${currentYear}`;
 
 const lastModifiedDate = document.lastModified;
 document.querySelector('#last-modified').textContent = `Date Last Modified ${lastModifiedDate}`;
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById("menu-button");
+    const navMenu = document.getElementById("nav-menu");
+
+    menuButton.addEventListener("click", function () {
+        // Toggle the menu visibility
+        if (navMenu.style.display === "block") {
+            navMenu.style.display = "none";
+        } else {
+            navMenu.style.display = "block";
+        }
+    });
+});
