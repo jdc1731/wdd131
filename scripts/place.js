012be4c6170e2ef0041b17548b39e2812dcf43fd
jdc1731 <jkeil@byupathway.edu>
2025-03-24 13:33:58 -0600

    added js

--- scripts/place.js
+++ scripts/place.js
@@ -4,3 +4,23 @@ document.querySelector('#current-year').textContent = `${currentYear}`;
 
 const lastModifiedDate = document.lastModified;
 document.querySelector('#last-modified').textContent = `Last Date Modified ${lastModifiedDate}`;
+
+const calculateWindChill = (tempC, speedKmh) => {
+    if (tempC <= 10 && speedKmh > 4.8) {
+        const windChill = 13.12 + 0.6215 * tempC - 11.37 * Math.pow(speedKmh, 0.16) + 0.3965 * tempC * Math.pow(speedKmh, 0.16);
+        return windChill;
+    }
+
+    else {
+        return 'N/A';
+    }
+}
+
+function displayWindChill() {
+    let tempC = parseFloat(document.getElementById('temp').textContent);
+    let speedKmh = parseFloat(document.getElementById('wind-speed').textContent);
+
+    let windChill = calculateWindChill(tempC, speedKmh);
+    document.getElementById('windchill').textContent = windChill;
+}
+document.addEventListener('DOMContentLoaded', displayWindChill);
